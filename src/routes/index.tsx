9eab3dcfb6acf1a28ed380526185e44a4a068d26
elianiva <git@elianiva.my.id>
2025-09-09 16:55:16 +0700

    feat: remove boilerplate with simple homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: App,
-})
+export const Route = createFileRoute("/")({
+	component: App,
+});
 
 function App() {
-  return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
-  )
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center bg-background">
+			<h1 className="text-8xl font-extralight text-slate-700 mb-4">SUMI</h1>
+			<p className="text-xl text-slate-500 font-light">
+				A design experimentation playground
+			</p>
+		</div>
+	);
 }
